44aa86738b6ab1771725e776872231dd9443202d
Tolerance <k03804@naver.com>
2017-10-08 15:48:32 +0900

    last commit

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
 
         // Do any additional setup after loading the view.
     }
@@ -22,6 +27,30 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
 //view controller to view controller연결 그 후 코드를 통해 버튼을 누르면 다른 뷰컨트롤러로 넘어가도록 함.
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+       selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC{
+            skillVC.player = player
+        }
+    }
+    //perpareForSegue is always called before viewDidLoad
     
    
     
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,23 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by 박관용 on 2017. 10. 8..
+//  Copyright © 2017년 Tolerance. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+    
+    
+    //var myData:String! --Implicitly unwarapped optional
+    //Use when you can guarantee variable will have data in it before used
+    
+    //var myData: String? --Optional
+    //Use when there may or may not be data in the variable at runtime
+    
+    
+}
